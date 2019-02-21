914fd1bd1664b8afbd3654ccfbaae85a3d8a32fd
Frank Jia <frankjia@Franks-MacBook-Pro.local>
2019-02-20 22:35:18 -0800

    Updates

---
+++ precache-manifest.46492ecb7fb272e6bf81aed4986618a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee2bbdde207989f9df09",
+    "url": "/CryptoDashboard/static/js/runtime~main.ee2bbdde.js"
+  },
+  {
+    "revision": "7fd98cb63b15ffc7a01d",
+    "url": "/CryptoDashboard/static/js/main.7fd98cb6.chunk.js"
+  },
+  {
+    "revision": "5c221784312ced5d1aac",
+    "url": "/CryptoDashboard/static/js/2.5c221784.chunk.js"
+  },
+  {
+    "revision": "7fd98cb63b15ffc7a01d",
+    "url": "/CryptoDashboard/static/css/main.2e46e8fa.chunk.css"
+  },
+  {
+    "revision": "5c221784312ced5d1aac",
+    "url": "/CryptoDashboard/static/css/2.59521556.chunk.css"
+  },
+  {
+    "revision": "0bc438f2c07594d57f352dc9c6b013c1",
+    "url": "/CryptoDashboard/index.html"
+  }
+];
